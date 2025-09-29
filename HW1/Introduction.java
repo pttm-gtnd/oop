7d2484c11bfebd6ed23faa14b578e0672a673458
24020242 <phamthitramy1306@gmail.com>
2025-09-29 21:47:23 +0700

    Update Introduction.java

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -1,4 +1,4 @@
-public class Introdution {
+public class Introduction {
   public static void main (String[] args){
     System out println ("Họ Tên:\t<Phạm Thị Trà My>\t" +
             "Mã SV:\t<24020242>\t" +
@@ -9,9 +9,9 @@ public class Introdution {
     for(int i = 9; i >=1; i--) {
       String bottleWorld = (i == 1) ? "bottle" : "bottles";
       System out println(i + " " + bottleWorld + "of beer on the wall, " + i + " " + bottleWorld + " of beer.");
-      System out println(Take one down, pass it around,);
+      System out println("Take one down, pass it around,");
     }
-    System out println(No more bottles of beer on the wall.);
+    System out println("No more bottles of beer on the wall.");
   }
 }
         
