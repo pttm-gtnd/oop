d920ea5a8cff78bbaa22e219e379f0210f0b92b4
24020242 <phamthitramy1306@gmail.com>
2025-09-30 10:57:51 +0700

    Create HowMany.java

---
+++ HW1/HowMany.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.*;
+public class HowMany {
+    public static void main(String[] args) {
+        int n = args.length;
+
+        System.out.print("You entered " + n + " command-line argument");
+        if(n == 1) {
+            System.out.println(".");
+        }
+        else {System.out.println("s.");
+        }
+    }
+}
